a9e0c6699a17f313a1fb347221a442270dac8afa
johnlloyd <johnlloydencierto@gmail.com>
2026-03-06 00:15:56 +0800

    revising the battle Controller

--- battController.java
+++ battController.java
@@ -1,22 +1,9 @@
 public class battController {
-    public void conductBattle(Hero player, Hero enemy, Dungeon level) {
-        System.out.println("Entering the " + level.theme + " dungeon...");
-        System.out.println(player.name + " encounters " + enemy.name + "!");
-        
-        while (player.health > 0 && enemy.health > 0) {
-            player.attack();
-            enemy.health -= 20; 
-            if (enemy.health <= 0) {
-                System.out.println(enemy.name + " has been defeated!");
-                break;
-            }
-
-            enemy.attack();
-            player.health -= 15; 
-            if (player.health <= 0) {
-                System.out.println(player.name + " has been defeated!");
-                break;
-            }
-        }
+    public void conductBattle(Hero h1, Hero h2, Map location){
+        System.out.println("Location: " + location.theme);
+        System.out.println("--FIGHT!--");
+        h1.attack();
+        h2.attack();
+        System.out.println("--BATTLE ENDED--");
     }
 }
