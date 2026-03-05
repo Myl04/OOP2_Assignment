528c9f8c76136cc62f015804bd9200b50cfeaaaa
milard <cabahugmilard@gmail.com>
2026-03-06 00:14:07 +0800

    adding the GameStats class

---
+++ Gamestats.java
@@ -0,0 +1,17 @@
+class GameStats {
+    private int exp = 0;
+
+    public void gainXP(int amount) {
+        exp += amount;
+        System.out.println("Gained " + amount + " XP! Total XP: " + exp);
+    }
+
+    public int getLevel() {
+        return exp / 100; 
+    }
+
+    public int getXP() {
+        return exp;
+    }
+
+}
